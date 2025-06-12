6eaeb12822eda25f7676ec615307381b45ac62da
yashchauhan66 <yashchauhan6660@gmail.com>
2025-06-12 19:52:02 +0530

    make responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import img from '../../public/gym1.jpg';
 // import img from '../assets/himal.jpg';
 
 export default function Navbar() {
-  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-        <img src={img} alt="Logo" className='logo' />
+          <img src={img} alt="Logo" className='logo' />
         </Link>
         
-        <div className="nav-links">
-          <Link to="/" className="nav-button">Home</Link>
-          <Link to="/login" className="nav-button">Login</Link>
-          <Link to="/signup" className="nav-button">Sign Up</Link>
-          <Link to="/about" className="nav-button">About</Link>
-          <Link to="/contact" className="nav-button">Contact</Link>
+        <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div className={`nav-links ${isOpen ? 'active' : ''}`}>
+          <Link to="/" className="nav-button" onClick={() => setIsOpen(false)}>Home</Link>
+          <Link to="/login" className="nav-button" onClick={() => setIsOpen(false)}>Login</Link>
+          <Link to="/signup" className="nav-button" onClick={() => setIsOpen(false)}>Sign Up</Link>
+          <Link to="/about" className="nav-button" onClick={() => setIsOpen(false)}>About</Link>
+          <Link to="/contact" className="nav-button" onClick={() => setIsOpen(false)}>Contact</Link>
         </div>
       </div>
     </nav>
